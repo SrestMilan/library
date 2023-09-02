8ee46bf5c7ac17db0db3ad2d4d3bc80d51a60d7a
SrestMilan <shrestham780@gmail.com>
2023-09-02 13:51:18 +0545

    new table added

--- librarymanagement.sql
+++ librarymanagement.sql
@@ -1,8 +1,10 @@
 -- query for library management system
 
--- creat
+-- created a database 
 CREATE DATABASE project;
 
+
+-- created a table for publisher
 CREATE TABLE table_publisher
 (
         PublisherName VARCHAR(100) PRIMARY KEY NOT NULL,
@@ -10,7 +12,16 @@ CREATE TABLE table_publisher
 		PublisherPhone VARCHAR(50) NOT NULL,
 );
 
+--created a table for book
 CREATE TABLE tbl_book (
 		BookID INT PRIMARY KEY NOT NULL IDENTITY (1,1),
 		Title VARCHAR(100) NOT NULL,
 		);
+
+-- created a table for borower
+CREATE TABLE borrower_tbl (
+		Borrower_CardNo INT PRIMARY KEY NOT NULL IDENTITY (100,1),-- start id with 100 and id increment with 1
+		Borrower_BorrowerName VARCHAR(100) NOT NULL,
+		Borrower_BorrowerAddress VARCHAR(200) NOT NULL,
+		Borrower_BorrowerPhone VARCHAR(50) NOT NULL,
+	);
